c7ce01908f46d83f5965b23a011110ea873c652e
themerrymarlin <dam134@case.edu>
2017-03-26 23:53:55 -0400

    finished places data manager

--- app/data_managers/places_data_manager.py
+++ app/data_managers/places_data_manager.py
@@ -6,16 +6,17 @@ from app.models.places import Places
 import base64
 from typing import Any
 
+
 class PlacesDataManager:
     ''' connect to mongo through init
         static methods for database connections
     '''
 
     def __init__(self):
-        self.client = MongoClient('localhost',27017) #localhost for now, may change to properties file if time permits
-        self.db = self.client.get_database('experimental1_2') #not sure what this is yet, using same as events
+        self.client = MongoClient('localhost',
+                                  27017)  # localhost for now, may change to properties file if time permits
+        self.db = self.client.get_database('experimental1_2')  # not sure what this is yet, using same as events
         self.place_collection = self.db.get_collection('locations')
-
 
     @staticmethod
     def convert_to_place_id(_id: ObjectId) -> str:
@@ -23,62 +24,61 @@ class PlacesDataManager:
         place_id = 'pl-' + b64_obj_id
         return place_id
 
-
     @staticmethod
     def convert_to_object_id(place_id: str) -> ObjectId:
         return ObjectId(base64.urlsafe_b64decode(place_id[3:]))
 
+    def create_empty_place(self) -> dict:
+        place_dict = {
+            'place_id': '',
+            'geo_coordinate': '',
+            'senti_score': '',
+            'place_type': '',
+            'keywords': '',
+            'deleted': False
+        }
+        return place_dict
 
-    def create_empty_place(self, place_id: str):
-        place_dict = Places().__dict__
-        place_dict['ObjectId']= PlacesDataManager.convert_to_object_id(place_id)
-        place_dict['deleted']=False
-        place_dict['type'] = 'place'
-        return Places()
-
-
-    def insert_one_place(self, place: Places) -> Places:
-        place_dict = place.__dict__
-        place_dict['deleted'] = False
-        place_dict['type'] = 'place'
+    def insert_one_place(self, place_dict: dict) -> dict:
         self.place_collection.insert_one(place_dict)
-        place.place_id = PlacesDataManager.convert_to_place_id(place_dict['_id'])
-        self.replace_one_place(place)
-        print('inserted place ' + place.place_id)
-        return place
+        place_dict['place_id'] = PlacesDataManager.convert_to_place_id(place_dict['_id'])
+        self.replace_one_place(place_dict)
+        print('inserted place' + place_dict['place_id'])
+        place_dict['_id'] = str(place_dict['_id'])
+        return place_dict
 
-
-    def replace_one_place(self, place: Places):
-        print('replacing place ' + place.place_id)
-        place_dict = place.__dict__
+    def replace_one_place(self, place_dict: dict):
+        print('replacing place ' + place_dict.place_id)
+        place_dict['_id'] = PlacesDataManager.convert_to_object_id(place_dict['place_id'])
         place_dict['deleted'] = False
         place_dict['type'] = 'place'
-        self.place_collection.replace_one({'_id': PlacesDataManager.convert_to_object_id(place.place_id)},place_dict)
-
+        self.place_collection.replace_one({'_id': PlacesDataManager.convert_to_object_id(place_dict['place_id'])}
+                                          , place_dict)
 
     def update_one_place(self, place_id: str, key: str, new_val: Any):
-        self.place_collection.update_one({'id': PlacesDataManager.convert_to_object_id(place_id)},{'$set':{key: new_val}})
-
+        self.place_collection.update_one({'id': PlacesDataManager.convert_to_object_id(place_id)},
+                                         {'$set': {key: new_val}})
 
     def delete_place(self, place_id: str):
-        self.place_collection.update_one({'_id': PlacesDataManager.convert_to_object_id(place_id)},{'$set':{'deleted': True}})
+        self.place_collection.update_one({'_id': PlacesDataManager.convert_to_object_id(place_id)},
+                                         {'$set': {'deleted': True}})
 
-
-    def find_place(self, place_id: str):
+    def find_place(self, place_id: str) -> dict:
         print('finding place ' + place_id)
-        place_dict = self.place_collection.find_one({'_id':PlacesDataManager.convert_to_object_id(place_id)})
+        place_dict = self.place_collection.find_one({'_id': PlacesDataManager.convert_to_object_id(place_id)})
         if place_dict is None:
             return None
         elif place_dict['deleted']:
             print(str(place_id) + ' has been deleted')
             return None
         else:
-            return Places.location_from_dict(place_dict)
-
+            place_dict['_id'] = str(place_dict['_id'])
+            return place_dict
 
     def find_all_places(self):
-        place_dicts = self.place_collection.find({'type':'place'})
+        place_dicts = self.place_collection.find({'type': 'place'})
         places = []
         for place_dict in place_dicts:
-            places.append(Places.location_from_dict(place_dict))
+            place_dict['_id'] = str(place_dict['_id]'])
+            places.append(place_dict)
         return places
