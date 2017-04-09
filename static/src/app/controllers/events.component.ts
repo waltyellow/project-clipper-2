f270eaba2123c0e03f50885c56bbe1ea038a664c
Eric Rinehart <edr27@case.edu>
2017-04-09 16:32:34 -0400

    Event list/map buttons change page content

--- static/src/app/controllers/events.component.ts
+++ static/src/app/controllers/events.component.ts
@@ -11,12 +11,18 @@ import { Eventx }        from '../models/eventx';
   templateUrl: '../templates/events.component.html',
 })
 export class EventsComponent implements OnInit {
+  public listView: boolean = true;
   public events: Eventx[];
+  
   constructor(private titleService: TitleService, private eventService: EventService) {
   }
 
   ngOnInit() {
     this.titleService.setTitle('Events');
     this.eventService.getEvents().subscribe(events => this.events = events['events'])
+  }
+  
+  setListView(listView: boolean) {
+  	 this.listView = listView;
   }
 }
