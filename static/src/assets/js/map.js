b74e91fa55f8cb5b639f9622292a505c42e7f1b2
Eric Rinehart <edr27@case.edu>
2017-04-11 14:55:44 -0400

    Added fake data to display in the map

--- static/src/assets/js/map.js
+++ static/src/assets/js/map.js
@@ -6,7 +6,25 @@ function generateMap() {
     var map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/light-v9',
-        center: [-90, 38],
-        zoom: 4
+        center: [-81.6944, 41.4933],
+        zoom: 10
+    });
+    
+    map.on('load', function() {
+        map.addSource('events', {
+            'type': 'geojson',
+            'data': 'assets/data/data.json'
+        });
+        
+        map.addLayer({
+            'id': 'events',
+            'source': 'events',
+            'type': 'circle',
+            'paint': {
+                'circle-opacity': 0.45,
+                'circle-color': '#00FF00',
+                'circle-radius': 10
+            }
+        }, 'waterway-label'); // before?
     });
 }
