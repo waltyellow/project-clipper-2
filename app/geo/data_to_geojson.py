1439a2e3869f88a8a7214972cc5011a9c9bf6aea
Eric Rinehart <edr27@case.edu>
2017-04-16 14:03:46 -0400

    Moved script so it can access data managers, added shell script to run geojsonizer

--- app/geo/data_to_geojson.py
+++ app/geo/data_to_geojson.py
@@ -2,6 +2,7 @@
 
 import geojson
 from geojson import FeatureCollection, Feature, Point
+from app.data_managers.event_data_manager import EventDataManager
 
 def create_feature_collection(lat, lon, ev_id):
     return FeatureCollection([create_feature(lat, lon, ev_id)])
@@ -10,4 +11,7 @@ def create_feature(lat, lon, ev_id):
     pt = Point((lat, lon))
     return Feature(geometry=pt, properties={'id': ev_id})
 
+edm = EventDataManager()
+events = edm.find_all_events()
+print(events)
 print(geojson.dumps(create_feature_collection(-81.6944, 41.4933, 'ev-56'), sort_keys=True))
