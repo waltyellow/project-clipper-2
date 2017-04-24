3c95279dd82a24cbcef1cd7d270afa64d7b1a8ea
Walter Huang <zxh108@case.edu>
2017-04-24 16:48:19 -0400

    tested webhook

--- app/utility/webhook_endpoint.py
+++ app/utility/webhook_endpoint.py
@@ -4,9 +4,6 @@ import json
 
 import logging
 logging.basicConfig(filename='webhook.log',level=logging.DEBUG)
-logging.debug('This message should go to the log file')
-logging.info('So should this')
-logging.warning('And this, too')
 
 @server.route('/webhook', methods=['POST'])
 def webhook():
