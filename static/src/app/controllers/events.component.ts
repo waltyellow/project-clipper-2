6ed6851136e757cca4253233b9b10ec8c1bd9ba2
Eric Rinehart <edr27@case.edu>
2017-04-26 21:41:20 -0400

    Implemented search functionality for events

--- static/src/app/controllers/events.component.ts
+++ static/src/app/controllers/events.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ViewEncapsulation} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { EventService } from '../services/event.service';
 import { TitleService } from '../services/title.service';
@@ -14,13 +15,25 @@ import { SortService } from '../services/sort.service';
 export class EventsComponent implements OnInit {
   public listView: boolean = true;
   public events: Eventx[];
+  public sub: any;
+  public searchString: string
 
-  constructor(private titleService: TitleService, private eventService: EventService, private sortService: SortService) {
+  constructor(private titleService: TitleService, private eventService: EventService, private sortService: SortService, private route: ActivatedRoute) {
   }
 
   ngOnInit() {
     this.titleService.setTitle('Events');
     this.eventService.getEvents().subscribe(events => this.events = events['events']);
+
+    this.sub = this.route.queryParams.subscribe(params => {
+        let sanitizedSearch = params['search']
+        this.eventService.getEvents(sanitizedSearch).subscribe(events => this.events = events['events']);
+    });
+  }
+
+  ngOnDestroy() {
+    // Clean sub to avoid memory leak
+    this.sub.unsubscribe();
   }
 
   public sortEventsByName() {
