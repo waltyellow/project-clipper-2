c996c72d47059b03009bd53918faf2b79ebaa1c7
Eric Rinehart <edr27@case.edu>
2017-04-26 22:42:21 -0400

    Added dynamic score calculation to conversion script; modified data point sizing for map

--- app/geo/data_to_geojson.py
+++ app/geo/data_to_geojson.py
@@ -3,17 +3,19 @@
 import geojson
 from geojson import FeatureCollection, Feature
 from app.data_managers.event_data_manager import EventDataManager
+from app.utility.action_handler import generate_dynamic_score_for_event
 
 
 def create_feature_collection(events):
     return FeatureCollection(list(map(create_feature, events)))
 
 def create_feature(event):
+    generate_dynamic_score_for_event(event)
     return Feature(geometry=event['geo_coordinates'], properties={
         'id': event['event_id'],
         'name': event['name'],
         'location': event['location'],
-        'excitement': event['senti_score']
+        'excitement': event['dynamic_senti_score']
     })
 
 edm = EventDataManager()
