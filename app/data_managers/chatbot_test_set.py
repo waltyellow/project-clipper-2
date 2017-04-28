fcb46f281892ef3e5c06cd499a68be83aa0d7787
Walter Huang <zxh108@case.edu>
2017-04-28 07:52:15 -0400

    fixed major backend bugs of insert giving errornous feedbacks

--- app/data_managers/chatbot_test_set.py
+++ app/data_managers/chatbot_test_set.py
@@ -3,5 +3,9 @@ from geojson import Point
 from app.data_managers.places_data_manager import PlaceDataManager
 from app.data_managers.event_data_manager import EventDataManager
 import time
+
 place_proto = places_data_manager.min_place_dict
+event_proto = event_data_manager.min_event_dict
+
+
 
