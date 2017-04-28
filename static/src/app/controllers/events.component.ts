ff83356deba3f3d61c1f3d7c9e54d774cecc21e6
relytred <tmayle@patriotsoftware.com>
2017-04-27 23:26:31 -0400

    removed sorting from events

--- static/src/app/controllers/events.component.ts
+++ static/src/app/controllers/events.component.ts
@@ -17,7 +17,6 @@ export class EventsComponent implements OnInit {
   public events: Eventx[];
   public sub: any;
   public searchText: string = "";
-  public sortOptionsVisible: boolean = false;
 
   constructor(private titleService: TitleService, private eventService: EventService, private sortService: SortService, private route: ActivatedRoute) {
   }
@@ -29,7 +28,6 @@ export class EventsComponent implements OnInit {
         let sanitizedSearch = params['search']
         this.eventService.getEvents(sanitizedSearch).subscribe(events => {
             this.events = events['events']
-            this.sortEventsByDate()
         });
     });
   }
@@ -39,45 +37,7 @@ export class EventsComponent implements OnInit {
     this.sub.unsubscribe();
   }
 
-  public sortEventsByName() {
-      this.sortService.propertySort<Eventx>(this.events, 'name');
-  }
-
-  public sortEventsByProximity() {
-      if (!!navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(position => {
-              this.events.sort((a, b) => {
-                  let d_a = this.distance(a, position)
-                  let d_b = this.distance(b, position)
-                  if (d_a < d_b) {
-                      return -1;
-                  } else if (d_a == d_b) {
-                      return 0;
-                  } else {
-                      return 1;
-                  }
-              });
-          });
-      }
-  }
-
-  distance(event, position) {
-      let long1 = event.geo_coordinates.coordinates[0]
-      let lat1 = event.geo_coordinates.coordinates[1]
-      let long2 = position.coords.longitude
-      let lat2 = position.coords.latitude
-      return Math.sqrt(Math.pow(long2 - long1, 2) + Math.pow(lat2 - lat1, 2));
-  }
-
-  public sortEventsByDate() {
-    this.sortService.propertySort<Eventx>(this.events, 'start_time', true);
-  }
-
   setListView(listView: boolean) {
   	 this.listView = listView;
-  }
-
-  public toggleDropdown() {
-      this.sortOptionsVisible = !this.sortOptionsVisible;
   }
 }
