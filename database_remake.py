3a42c8b60c412ca1e91ede8aed72dc441802edb8
Walter Huang <zxh108@case.edu>
2017-05-02 19:02:08 -0400

    added database remaker

--- database_remake.py
+++ database_remake.py
@@ -1,10 +1,9 @@
 import sys
-sys.path.append('/root/project-clipper-2')
-
 from pymongo import MongoClient
 from app.data_managers import chatbot_test_set, common
 from app.test_data import generate_events_and_cb, cb_and_sl, restaurants
 
+sys.path.append('/root/project-clipper-2')
 client = MongoClient('localhost', 27017)
 client.drop_database(common.database_name)
 generate_events_and_cb.create_campus_buildings_and_events()
